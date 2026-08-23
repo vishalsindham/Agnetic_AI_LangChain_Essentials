23f475a8b5baa2c57ea973f1aaf6763993ebe760
Vishal Sindham <vishalsindhamwork@gmail.com>
2026-08-23 11:26:40 +0530

    Uncomment the lines.

--- tools.py
+++ tools.py
@@ -11,44 +11,44 @@ from langchain.agents import create_agent
 
 load_dotenv()
 
-# @tool
-# def real_number_calculator(
-#     a:float, b:float, operation: Literal["add", "subtract", "multiply", "divide"]
-# ) -> float:
-#     """Perform basic arithmetic operations on two real numbers."""
-#     print("Invoking calculator tool")
-#     # Perform the specified operation
-#     if operation == "add":
-#         return a + b
-#     elif operation == "subtract":
-#         return a - b
-#     elif operation == "multiply":
-#         return a * b
-#     elif operation == "divide":
-#         if b == 0:
-#             raise ValueError("Division by zero if not allowed.")
-#         return a/b
-#     else:
-#         raise ValueError(f"Invalid operation : {operation}.")
+@tool
+def real_number_calculator(
+    a:float, b:float, operation: Literal["add", "subtract", "multiply", "divide"]
+) -> float:
+    """Perform basic arithmetic operations on two real numbers."""
+    print("Invoking calculator tool")
+    # Perform the specified operation
+    if operation == "add":
+        return a + b
+    elif operation == "subtract":
+        return a - b
+    elif operation == "multiply":
+        return a * b
+    elif operation == "divide":
+        if b == 0:
+            raise ValueError("Division by zero if not allowed.")
+        return a/b
+    else:
+        raise ValueError(f"Invalid operation : {operation}.")
 
-# agent = create_agent(
-#     model="google_genai:gemini-3.1-flash-lite",
-#     tools=[real_number_calculator],
-#     system_prompt="You are a helpful assistant."
-# )
+agent = create_agent(
+    model="google_genai:gemini-3.1-flash-lite",
+    tools=[real_number_calculator],
+    system_prompt="You are a helpful assistant."
+)
 
-# # Invoking the agent using real numbers, which makes the tool call.
-# response = agent.invoke(
-#     {"messages" : [{"role":"user", "content":"what is 3.125 * 4.1234..?"}]}
-# )
+# Invoking the agent using real numbers, which makes the tool call.
+response = agent.invoke(
+    {"messages" : [{"role":"user", "content":"what is 3.125 * 4.1234..?"}]}
+)
 
-# print(response["messages"][-1].content[0]['text'])
+print(response["messages"][-1].content[0]['text'])
 
 # Invoking the agent with word numbers to check if the agent makes a tool call or not. Tool description mentions
 # perform basic arithmetic operations on rel numbers. 
-# response = agent.invoke({"messages" : [{"role":"user", "content": "What is three multiplied by four."}]})
+response = agent.invoke({"messages" : [{"role":"user", "content": "What is three multiplied by four."}]})
 
-# print(response["messages"][-1].content[0]['text'])
+print(response["messages"][-1].content[0]['text'])
 
 
 ## If there is a tool which is shared standard tool and needs a agent-specific instructions.
